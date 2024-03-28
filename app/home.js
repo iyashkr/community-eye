5b874279732919c690742c31fadd441953e2be81
iyashkr <kumaryash1212@gmail.com>
2024-03-29 00:21:46 +0530

    home page done

---
+++ app/home.js
@@ -0,0 +1,132 @@
+import { View, Text, ScrollView, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { router } from 'expo-router';
+
+export default function Home() {
+    return (
+        <ScrollView style={styles.container}>
+            <Image source={require('../assets/images/public/pmalogo.png')}
+                style={{ height: 96, width: 96, alignSelf: 'center' }}>
+            </Image>
+            <View>
+                <View style={{ gap: 10, paddingHorizontal: 30, backgroundColor: "#428FE3", height: 120, borderRadius: 5 }}>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between', paddingTop: 20 }}>
+                        <Text style={{ fontWeight: 600 }}>Total Case Solved till Date ✅</Text>
+                        <Text style={{ color: '#e30d0d', fontWeight: 700 }}>- LIVE</Text>
+                    </View>
+                    <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+                        <View style={styles.numbers}><Text style={{ fontSize: 15, fontWeight: 600, color: 'white' }}>0</Text></View>
+                        <View style={styles.numbers}><Text style={{ fontSize: 15, fontWeight: 600, color: 'white' }}>0</Text></View>
+                        <View style={styles.numbers}><Text style={{ fontSize: 15, fontWeight: 600, color: 'white' }}>0</Text></View>
+                        <View style={styles.numbers}><Text style={{ fontSize: 15, fontWeight: 600, color: 'white' }}>0</Text></View>
+                        <View style={styles.numbers}><Text style={{ fontSize: 15, fontWeight: 600, color: 'white' }}>0</Text></View>
+                        <View style={styles.numbers}><Text style={{ fontSize: 15, fontWeight: 600, color: 'white' }}>0</Text></View>
+                        <View style={styles.numbers}><Text style={{ fontSize: 15, fontWeight: 600, color: 'white' }}>0</Text></View>
+                        <View style={styles.numbers}><Text style={{ fontSize: 15, fontWeight: 600, color: 'white' }}>0</Text></View>
+                    </View>
+                </View>
+                <TouchableOpacity activeOpacity={0.8} style={styles.yellowBar} onPress={() => router.navigate('/complaintStatus')}>
+                    <Text style={{ fontSize: 16, color: 'white', fontWeight: 600, }}>Check Complaint Status</Text>
+                </TouchableOpacity>
+            </View>
+            <Text style={{ fontSize: 16, fontWeight: 500 }}>Select your problem, because we've got the solution!</Text>
+            <View style={{ marginVertical: 15, gap: 15 }}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', elevation: 5 }} activeOpacity={0.7}>
+                        <Image source={require('../assets/images/public/damagedRoads.png')} style={{ width: 160, height: 150 }}></Image>
+                        <View style={styles.categoryTextContainer}>
+                            <Text style={[styles.categoryText, { fontWeight: 600 }]}>Damaged Roads</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', elevation: 5 }} activeOpacity={0.7}>
+                        <Image source={require('../assets/images/public/missingSignBoards.png')} style={{ width: 160, height: 150 }}></Image>
+                        <View style={styles.categoryTextContainer}>
+                            <Text style={[styles.categoryText, { fontWeight: 600 }]}>Missing Sign Boards</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', elevation: 5 }} activeOpacity={0.7}>
+                        <Image source={require('../assets/images/public/accidentProneArea.png')} style={{ width: 160, height: 150 }}></Image>
+                        <View style={styles.categoryTextContainer}>
+                            <Text style={[styles.categoryText, { fontWeight: 600 }]}>Accident Prone Area</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', elevation: 5 }} activeOpacity={0.7}>
+                        <Image source={require('../assets/images/public/infrastructure.png')} style={{ width: 160, height: 150 }}></Image>
+                        <View style={styles.categoryTextContainer}>
+                            <Text style={[styles.categoryText, { fontWeight: 600 }]}>Infrastructure</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', elevation: 5 }} activeOpacity={0.7}>
+                        <Image source={require('../assets/images/public/accidentProneArea.png')} style={{ width: 160, height: 150 }}></Image>
+                        <View style={styles.categoryTextContainer}>
+                            <Text style={[styles.categoryText, { fontWeight: 600 }]}>Accident Prone Area</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', elevation: 5 }} activeOpacity={0.7}>
+                        <Image source={require('../assets/images/public/infrastructure.png')} style={{ width: 160, height: 150 }}></Image>
+                        <View style={styles.categoryTextContainer}>
+                            <Text style={[styles.categoryText, { fontWeight: 600 }]}>Infrastructure</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-around' }}>
+                    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', elevation: 5 }} activeOpacity={0.7}>
+                        <Image source={require('../assets/images/public/accidentProneArea.png')} style={{ width: 160, height: 150 }}></Image>
+                        <View style={styles.categoryTextContainer}>
+                            <Text style={[styles.categoryText, { fontWeight: 600 }]}>Accident Prone Area</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={{ alignItems: 'center', justifyContent: 'center', elevation: 5 }} activeOpacity={0.7}>
+                        <Image source={require('../assets/images/public/infrastructure.png')} style={{ width: 160, height: 150 }}></Image>
+                        <View style={styles.categoryTextContainer}>
+                            <Text style={[styles.categoryText, { fontWeight: 600 }]}>Infrastructure</Text>
+                        </View>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        width: "100%",
+        height: "100%",
+        paddingHorizontal: 24,
+        backgroundColor: "#F5F9FD"
+    },
+    numbers: {
+        borderColor: '#2E7BCF',
+        borderWidth: 2,
+        height: 27, width: 27,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 5,
+        backgroundColor: "#428FE3"
+    },
+    yellowBar: {
+        borderRadius: 5,
+        backgroundColor: "#FCB226",
+        width: "60%", height: 48,
+        alignSelf: 'center',
+        paddingHorizontal: 20,
+        justifyContent: 'center',
+        alignItems: 'center',
+        bottom: 24
+    },
+    categoryTextContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#4087d6',
+        height: 31, width: "100%",
+        borderRadius: 5
+    },
+    categoryText: {
+        color: 'white',
+        fontSize: 14,
+    }
+});
--- app/index.js
+++ app/index.js
@@ -1,32 +1,35 @@
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { DigiLockerIcon, ShieldIcon } from '../components/icons'
+import { router } from 'expo-router'
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <View style={{ alignItems: 'center', marginVertical: 40 }}>
-        <Image source={require('../assets/images/public/pmalogo.png')}></Image>
+      <View style={{ alignItems: 'center', marginVertical: 30 }}>
+        <Image source={require('../assets/images/public/logowithtext.png')}></Image>
       </View>
       <View style={{ alignItems: 'center', marginBottom: 30 }}>
         <Text style={{ fontSize: 20 }}>Let's get started</Text>
       </View>
-      <TouchableOpacity style={{ gap: 15, flexDirection: 'row', alignItems: 'center', elevation: 2, height: 64, justifyContent: 'center', backgroundColor: 'white', marginBottom: 30 }}>
+      <TouchableOpacity style={styles.digiBtn} activeOpacity={0.7}>
         <DigiLockerIcon />
         <Text style={{ fontSize: 18 }}>Continue with E-AADHAR</Text>
       </TouchableOpacity>
-      <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center', justifyContent: 'center', marginBottom: 30 }}>
+      <View style={{ flexDirection: 'row', gap: 10, alignItems: 'center', justifyContent: 'center', marginBottom: 40 }}>
         <View style={{ borderBottomWidth: 1, width: "32%", borderColor: "#AAAAAA" }}></View>
         <Text style={{ color: "#AAAAAA" }}>or continue with</Text>
         <View style={{ borderBottomWidth: 1, width: "32%", borderColor: "#AAAAAA" }}></View>
       </View>
       <Text style={{ fontSize: 16, color: "#6E6E6E", marginBottom: 10 }}>Aadhar Number</Text>
-      <TextInput placeholder='1234-5678-8765-4321' style={{ backgroundColor: 'white', height: 48, elevation: 2, paddingHorizontal: 15 }}></TextInput>
+      <TextInput placeholder='1234-5678-8765-4321'
+        style={{ backgroundColor: 'white', height: 48, elevation: 2, paddingHorizontal: 15 }}>
+      </TextInput>
       <View style={{ gap: 10, flexDirection: 'row', marginTop: 20, justifyContent: 'center', alignItems: 'center' }}>
         <ShieldIcon />
         <Text style={{ flex: 1, color: "#828282", fontSize: 12, }}>By clicking "Continue", you agree to the Terms and Privacy Policy</Text>
       </View>
-      <TouchableOpacity style={{ marginTop: 30, backgroundColor: '#ffb831', width: '50%', height: 48, alignSelf: 'center', justifyContent: 'center', elevation: 2 }}>
+      <TouchableOpacity style={styles.loginBtn} activeOpacity={0.7} onPress={() => router.navigate('/home')}>
         <Text style={{ fontSize: 16, fontWeight: 700, color: 'white', textAlign: 'center' }}>LogIn</Text>
       </TouchableOpacity>
     </View>
@@ -40,4 +43,21 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     backgroundColor: "#F5F9FD"
   },
+  loginBtn: {
+    marginTop: 30,
+    backgroundColor: '#ffb831',
+    width: '50%',
+    height: 48,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    elevation: 2
+  },
+  digiBtn: {
+    gap: 15, flexDirection: 'row',
+    alignItems: 'center',
+    elevation: 2, height: 64,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    marginBottom: 40
+  }
 })
